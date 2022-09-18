28c092d4fef7c4fb6a2d7a54a67ea42b074a95e4
gomoriyb <gomoriyb@mail.uc.edu>
2022-09-18 19:03:19 -0400

    Completing Lab 3

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,7 +1,26 @@
 ﻿function getIP{
     (Get-NetIPAddress).IPv4Address | Select-String "192*"
 }
-Write-Host(getIP)
+function getDayOfWeek{
+    (Get-Date).DayOfWeek
+}
+function getYear{
+    (Get-Date).Year
+}
+function getMonth{
+    (Get-Date).Month
+}
+function getDay{
+    (Get-Date).Day
+}
 $IP = getIP
-Write-Host("This machine's IP is $IP")
-Write-Host("This machine's IP is {0}" -f $IP)
+$User = $env:USERNAME
+$Hostname = $env:USERDOMAIN
+$PowerShellMajorVersion = $Host.Version.Major
+$DayOfWeek = getDayOfWeek
+$Year = getYear
+$Month = getMonth
+$Day = getDay
+$Body = "This machine's IP is $IP. User is $User. Hostname is $Hostname. PowerShell Version is $PowerShellMajorVersion. Today's date is $DayOfWeek, $Month/$Day/$Year."
+$Body | out-file C:\it3038c-scripts\powershell\script_output.txt
+Write-Host($Body)
